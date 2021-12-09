ed3c15cef05c77fae93d6c42edc58b3efe675de8
AngelSony <angelfol@outlook.com>
2021-12-09 19:17:39 -0300

    Semillas

---
+++ TPFinalJava/src/entities/Semilla.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class Semilla {
+
+	private int cod_semilla;
+	private String raza;
+	private String especie;
+	
+	public int getCod_semilla() {
+		return cod_semilla;
+	}
+	public void setCod_semilla(int cod_semilla) {
+		this.cod_semilla = cod_semilla;
+	}
+	public String getRaza() {
+		return raza;
+	}
+	public void setRaza(String raza) {
+		this.raza = raza;
+	}
+	public String getEspecie() {
+		return especie;
+	}
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+	
+	
+}
