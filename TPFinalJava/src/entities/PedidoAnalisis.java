11d2ebdc78a08e6abf8cef8e4eddeeb750746883
AngelSony <angelfol@outlook.com>
2021-12-09 20:15:30 -0300

    getters y setters

--- TPFinalJava/src/entities/PedidoAnalisis.java
+++ TPFinalJava/src/entities/PedidoAnalisis.java
@@ -6,4 +6,23 @@ public class PedidoAnalisis {
 	private String estado;
 	private String observaciones;
 	
+	public Analisis getAnalisis() {
+		return analisis;
+	}
+	public void setAnalisis(Analisis analisis) {
+		this.analisis = analisis;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getObservaciones() {
+		return observaciones;
+	}
+	public void setObservaciones(String observaciones) {
+		this.observaciones = observaciones;
+	}
+	
 }
