4023f4cbb5f0dc649fe56b102923fe0541608c55
AngelSony <angelfol@outlook.com>
2021-12-09 19:19:31 -0300

    Cliente

---
+++ TPFinalJava/src/entities/Cliente.java
@@ -0,0 +1,36 @@
+package entities;
+
+public class Cliente {
+
+	private String cuit;
+	private String razonSocial;
+	private String telefono;
+	private String email;
+	
+	public String getCuit() {
+		return cuit;
+	}
+	public void setCuit(String cuit) {
+		this.cuit = cuit;
+	}
+	public String getRazonSocial() {
+		return razonSocial;
+	}
+	public void setRazonSocial(String razonSocial) {
+		this.razonSocial = razonSocial;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
