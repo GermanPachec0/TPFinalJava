eb674e2f6a3f60e629e266e1e26b34b96eacbff5
German <germanpacheco98@gmail.com>
2021-12-08 23:37:14 -0300

    3

--- TPFinalJava/src/entities/prueba.java
+++ TPFinalJava/src/entities/prueba.java
@@ -4,5 +4,9 @@ public class prueba {
 
 	//COMIMMIT
 	//HOLAHOLA
+<<<<<<< Upstream, based on branch 'master' of https://github.com/GermanPachec0/TPFinalJava.git
 	//2
+=======
+	//3
+>>>>>>> 1b05fda 3
 }
