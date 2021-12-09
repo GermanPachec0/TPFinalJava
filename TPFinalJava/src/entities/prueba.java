56cf09b046b427ecef2d8f8049029ecd4e1bd3c6
German <germanpacheco98@gmail.com>
2021-12-08 23:46:20 -0300

    merge

--- TPFinalJava/src/entities/prueba.java
+++ TPFinalJava/src/entities/prueba.java
@@ -4,9 +4,8 @@ public class prueba {
 
 	//COMIMMIT
 	//HOLAHOLA
-<<<<<<< Upstream, based on branch 'master' of https://github.com/GermanPachec0/TPFinalJava.git
 	//2
-=======
 	//3
->>>>>>> 1b05fda 3
+	//3
+	//3
 }
