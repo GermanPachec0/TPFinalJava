681e080c14158a6a70440627d5de5f6047e926fe
AngelSony <angelfol@outlook.com>
2021-12-09 19:23:49 -0300

    Usuario

---
+++ TPFinalJava/src/entities/Usuario.java
@@ -0,0 +1,49 @@
+package entities;
+
+public class Usuario {
+
+	private int cod_user;
+	private String username;
+	private String password;
+	private int tipo;
+	private String nombre;
+	private String apellido;
+	
+	public int getCod_user() {
+		return cod_user;
+	}
+	public void setCod_user(int cod_user) {
+		this.cod_user = cod_user;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getTipo() {
+		return tipo;
+	}
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+}
