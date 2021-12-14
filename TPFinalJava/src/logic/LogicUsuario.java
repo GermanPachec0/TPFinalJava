d3516a13e36212e236dc7d2a1e796d7df5134f71
AngelSony <angelfol@outlook.com>
2021-12-13 21:05:22 -0300

    Completado logica de usuarios

--- TPFinalJava/src/logic/LogicUsuario.java
+++ TPFinalJava/src/logic/LogicUsuario.java
@@ -11,16 +11,23 @@ public class LogicUsuario {
 	public LogicUsuario() {
 		du = new DataUsuario();
 	}
-	
 	public LinkedList<Usuario> getAll(){
 		return du.getAll();
 	}
-	
 	public Usuario getBycod(Usuario u) {
 		return du.getByCod(u);
 	}
-	
-	public Usuario getByUser(Usuario u) {
+	public void add(Usuario u) {
+		du.add(u);
+	}
+	public void update(Usuario u) {
+		du.update(u);
+	}
+	public void remove(Usuario u) {
+		du.remove(u);
+	}
+	public Usuario validate(Usuario u) {
+		//Agregar encriptación de password
 		return du.getByUser(u);
 	}
 }
